425f008464795d170a46335d377803e90f537505
Tima <timkacofe9480@gmail.com>
2025-12-04 13:31:43 +0300

    lolo

---
+++ script.js
@@ -0,0 +1,30 @@
+//Задание 1
+const concatenateStrings = (spring, spring2) => {
+    let result = (spring.trim() + spring2.trim())
+    return result;
+}
+//Задание 2
+const getStringLength = (str) => {
+    return str.length;
+}
+//Задание 3
+const getStringFromTemplate = (firstName, lastName) => {
+    return `Hello, ${firstName} ${lastName}!`;
+}
+//Задание 4
+const getChar = (str, ind) => {
+    return str[ind - 1];
+}
+//Задание 5
+const removeFirstOccurrences = (str, subString) => {
+    const ind = str.indexOf(subString);
+    return str.slice(0, ind) + str.slice(ind + subString.length);
+}
+//Задание 6
+const getRectangleString = (width, height) => {
+    let result = '';
+    for (let i = 0; i < height; i++) {
+        result += '*'.repeat(width) + '\n';
+    }
+    return result.trim();
+}
